cee0abcf7f6756150075eae6c270e6854452363e
Youssafouhba <youssefouhba@gmail.com>
2024-03-10 17:25:39 +0000

    first commit

---
+++ src/main/java/com/crud/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.crud;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", value = "/hh")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+    }
+}
